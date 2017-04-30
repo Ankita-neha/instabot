fc2242eee2fce1a913b46f2731ad71130a0f17de
Ankita-neha <ak813174@gmail.com>
2017-04-30 21:11:25 +0530

    function for fetching the another user's details

--- instabot.py
+++ instabot.py
@@ -3,9 +3,11 @@ import requests          # Here importing the requests library which is installe
 APP_ACCESS_TOKEN = '1599633091.2fc0da1.63f5a1608a3e4414b4d96117bca38027'
 BASE_URL = 'https://api.instagram.com/v1/'
 
+
 def self_info():         # this is the function which fetches my instagram profile details by using api.
+
     request_url = (BASE_URL +'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
-    print 'REQUESTING URL FOR DATA : ' + request_url
+    print 'REQUESTING URL FOR DATA  : ' + request_url
     my_info = requests.get(request_url).json()
     print 'MY INFO IS: \n'
     print 'My Bio: ' + my_info['data']['bio']
@@ -17,5 +19,27 @@ def self_info():         # this is the function which fetches my instagram profi
 
 self_info()
 
+
+#this function is used to fetch another user's details by entering instagram username and also it will return the userID
+def get_user_by_username(insta_username):
+
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'REQUESTING URL FOR DATA  : ' + request_url
+
+    search_results = requests.get(request_url).json()
+
+    print search_results
+
+    if search_results['meta']['code'] == 200:
+        if len(search_results['data']) > 0:
+            print 'User ID: ' + search_results['data'][0]['id']
+            return search_results['data'][0]['id']
+        else:
+            print 'User does not found!'
+    else:
+        print 'Status code other than 200 was received'
+    return None
+
+get_user_by_username('shubham.is.here')
 
 
